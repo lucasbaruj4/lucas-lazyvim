e3a4a9971cb27860304352a1aaf998a81a040f84
lucasbaruj4 <barujalucas0@gmail.com>
2025-09-15 11:49:00 -0300

    changes with autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -2,29 +2,56 @@
 return {
   {
     "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- Optional command to toggle auto-save
     event = { "InsertLeave", "TextChanged" }, -- Load on these events
     opts = {
       enabled = true, -- Enable auto-save on startup
       execution_message = {
-        enabled = false, -- Disable save notifications for cleaner experience
+        enabled = true, -- Enable save notifications to see when files are saved
+        message = function() -- Message function for save notifications
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
       },
       trigger_events = { -- Events that trigger auto-save
         immediate_save = { "BufLeave", "FocusLost" }, -- Save immediately on these events
         defer_save = { "InsertLeave", "TextChanged" }, -- Save with debounce on these events
-        cancel_defered_save = { "InsertEnter" }, -- Cancel pending saves on these events
+        cancel_deferred_save = { "InsertEnter" }, -- Cancel pending saves on these events
       },
       debounce_delay = 1000, -- Delay in ms before saving (1 second)
+      -- Simplified condition without requiring utils
       condition = function(buf)
         local fn = vim.fn
-        local utils = require("auto-save.utils.data")
+        local buftype = fn.getbufvar(buf, "&buftype")
+        local filetype = fn.getbufvar(buf, "&filetype")
 
-        -- Don't save if file doesn't exist yet
-        if fn.getbufvar(buf, "&modifiable") == 1 and
-           utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+        -- Don't save special buffers
+        if buftype ~= "" then
+          return false
+        end
+
+        -- Don't save certain filetypes
+        local excluded_filetypes = {
+          "neo-tree",
+          "neo-tree-popup",
+          "notify",
+          "TelescopePrompt",
+        }
+
+        for _, ft in ipairs(excluded_filetypes) do
+          if filetype == ft then
+            return false
+          end
+        end
+
+        -- Check if buffer is modifiable
+        if fn.getbufvar(buf, "&modifiable") == 1 then
           return true
         end
+
         return false
       end,
+      write_all_buffers = false, -- Write only current buffer
+      clean_command_line_interval = 0, -- Don't show save message in command line
     },
   },
 }
